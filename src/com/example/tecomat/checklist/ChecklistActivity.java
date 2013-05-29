6878ba19bf07736c088749de4595935e737c4af5
Rodrigo Melo <rodrigodma@gmail.com>
2013-05-29 01:39:13 -0300

    primeira tela finalizada, preparando a transição de tela e persistencia dos dados

--- src/com/example/tecomat/checklist/ChecklistActivity.java
+++ src/com/example/tecomat/checklist/ChecklistActivity.java
@@ -1,8 +1,13 @@
 package com.example.tecomat.checklist;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChecklistActivity extends Activity {
 
@@ -10,6 +15,23 @@ public class ChecklistActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
+        
+        final EditText textNomeObra = (EditText) findViewById(R.id.etNovaObra);
+        final EditText textQtdeBalanca = (EditText) findViewById(R.id.etQtdeBalanca);
+        final EditText textDataInicio = (EditText) findViewById(R.id.etDataInicio);
+        
+        Button button = (Button) findViewById(R.id.btCriarObra);
+        
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String nObra = textNomeObra.getText().toString();
+				int qtdeBalanca = Integer.parseInt(textQtdeBalanca.getText().toString());
+				int dtInicio = Integer.parseInt(textDataInicio.getText().toString());
+				
+			}
+		});
     }
 
     @Override
